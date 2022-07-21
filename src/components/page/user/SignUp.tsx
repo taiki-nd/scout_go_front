b1753b32910eb85fb45a90a17d9c1dc48684c20c
taiki-nd <kii.develop.0517@gmail.com>
2022-07-22 04:30:28 +0900

    エラーハンドリング

--- src/components/page/user/SignUp.tsx
+++ src/components/page/user/SignUp.tsx
@@ -3,25 +3,43 @@ import { AiFillGithub, AiFillGoogleCircle } from 'react-icons/ai'
 import { useState, useCallback, SyntheticEvent } from "react";
 import { auth } from "../../../utils/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { errorMessageConstants } from "../../../config/constant";
+import { passwordPattern } from "../../../utils/regularExpressions";
 
 export const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [passwordState, setPasswordState] = useState(true);
+  const [authMessage, setAuthMessage] = useState("")
 
+  /**
+   * submit
+   * user登録処理
+   */
   const submit = useCallback(async (e: SyntheticEvent) => {
     e.preventDefault();
 
+    // passwordの確認
     if (password !== passwordConfirm) {
-      console.log("password does not match")
-      setPasswordState(false);
+      setAuthMessage(errorMessageConstants.passwordMatchingError);
+    } else if (!password.match(passwordPattern) || !passwordConfirm.match(password)) {
+      console.log(password.match(passwordPattern))
+      setAuthMessage(errorMessageConstants.passwordPatternError)
     } else {
+      // firebaseへのuser登録
       try {
         const res = await createUserWithEmailAndPassword(auth, email, password);
         console.log('res: ', res);
-      } catch (e) {
-        console.error(e);
+      } catch (e :any) {
+        console.error('firebase error code:', e.code);
+        switch (e.code) {
+          case 'auth/email-already-in-use':
+            setAuthMessage(errorMessageConstants.firebaseEmailExistError);
+            break;
+          case 'auth/invalid-email':
+            setAuthMessage(errorMessageConstants.firebaseInvalidEmailError);
+            break;
+        }
       }
     }
 
@@ -39,9 +57,9 @@ export const SignUp = () => {
           </div>
           <div className="or-seperator"><b>or</b></div>
           {
-            passwordState ? 
+            authMessage == '' ? 
             <div className="alert alert-light" role="alert">write info below</div> 
-            : <div className="alert alert-danger" role="alert">password does not match</div>
+            : <div className="alert alert-danger" role="alert">{authMessage}</div>
           }
           <div className="form-group">
             <input type="email" className="form-control input-lg" name="email" placeholder="Email Address" required
@@ -49,12 +67,14 @@ export const SignUp = () => {
             />
           </div>
           <div className="form-group">
-            <input type="password" className="form-control input-lg" name="password" placeholder="Password" required
+            <input type="password" className="form-control input-lg" name="password" placeholder="Password"
+              required pattern="(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{6,}" title="半角英大文字・半角英小文字・半角数字いれずれも必ず含む6文字以上"
               onChange={e => setPassword(e.target.value)}
             />
           </div>
           <div className="form-group">
             <input type="password" className="form-control input-lg" name="confirm_password" placeholder="Confirm Password" required
+              pattern="(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{6,}" title="半角英大文字・半角英小文字・半角数字いれずれも必ず含む6文字以上"
               onChange={e => setPasswordConfirm(e.target.value)}
             />
           </div>
---
+++ src/utils/regularExpressions.ts
@@ -0,0 +1,1 @@
+export const passwordPattern = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{6,}$/
