9642b975919e8bb155231dbda28255552983126e
taiki-nd <kii.develop.0517@gmail.com>
2022-08-08 22:54:16 +0900

    footerの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/part/Header';
+import { Footer } from './components/part/Footer';
 import { SignUp } from './components/page/user/signup/SignUp';
 import { SignUp2 } from './components/page/user/signup/SignUp2';
 import { SignIn } from './components/page/user/signin/SignIn';
@@ -12,17 +13,13 @@ function App() {
         <BrowserRouter>
           <Header />
           <main className="container">
-
             <Routes>
               <Route path="/signup" element={<SignUp />} />
               <Route path="/signup2" element={<SignUp2 />} />
               <Route path="/signin" element={<SignIn />} />
             </Routes>
-
-            {
-              // footer
-            }
           </main>
+          <Footer />
         </BrowserRouter>
       </body>
     </>
